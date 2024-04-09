442a9fd74c595adf5cf4ab99249edb2798dbabf0
Vit <mayllby@gmail.com>
2024-04-09 19:07:47 +0300

    Добавил код

---
+++ PythonTest/NotePad/Reading.py
@@ -0,0 +1,17 @@
+# Промежуточная контрольная работа по блоку специализация
+# Урок 1. Приложение заметки (Python)
+# https://gb.ru/lessons/432765/homework
+
+'''
+Чтение
+'''
+class Reading:
+	
+    def reade(): 
+        spros = []
+        with open('phonebook.txt','r', encoding='utf-8') as Phone:
+            for i in Phone.readlines():
+                spros.append(i.strip().split(',')) 	
+        return (spros)
+    # spros = reade()
+    print(reade())
