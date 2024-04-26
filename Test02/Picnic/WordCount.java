74c02beaecc0086e9ae380e95bee1731536b7634
Vit <mayllby@gmail.com>
2024-04-26 12:46:52 +0300

    Добавил код

---
+++ Test02/Picnic/WordCount.java
@@ -0,0 +1,25 @@
+/*
+Промежуточная контрольная работа по блоку специализация
+Урок 2. Виртуальный пикник (Java)
+https://gb.ru/lessons/432766/homework
+
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * WordCount
+ * Посчёт количества слов в тексте
+ */
+public class WordCount {
+	public static void wordCount(List<String> listWords) {
+
+		List<String> list = new ArrayList<>("input.txt"); 
+		int count = 0; 
+		while (iterator.hasNext()) { 
+			count++;
+		}
+		System.out.printf("Words count: " + count);
+	}
+}
