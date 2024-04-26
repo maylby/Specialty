e02880762f5790675200407160d164123206f192
Vit <mayllby@gmail.com>
2024-04-26 13:00:30 +0300

    Добавил код

---
+++ Test02/Picnic/MatchWords.java
@@ -0,0 +1,34 @@
+/*
+Промежуточная контрольная работа по блоку специализация
+Урок 2. Виртуальный пикник (Java)
+https://gb.ru/lessons/432766/homework
+
+ */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+ * MatchWords
+ * подсчёт и печать повторяющихся слов
+ */
+pablic class MatchWords {
+
+	private static void matchWords(List<String> listWords) {
+		List<String> sortedWords = new ArrayList<>(listWords);
+		Collections.sort(sortedWords);
+		int count = 1;
+		String currentWord = sortedWords.get(0); 
+		for (int i = 1; i < sortedWords.size(); i++) {
+			if (sortedWords.get(i).equals(currentWord)) {
+				count++;
+			} else {
+				System.out.println(currentWord + " -> " + count); 
+				count = 1;
+				currentWord = sortedWords.get(i);
+			}
+		}
+		System.out.println(currentWord + " -> " + count); // вывод последнего слова, 
+								                          // до которого цикл не дойдёт
+	}
+}
